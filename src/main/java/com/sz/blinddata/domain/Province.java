dfe78b02a8694cafd7c6ab63376b8225029c53a6
wjz <25348903334@qq.com>
2020-02-29 11:45:01 +0800

    添加实体类Province

---
+++ src/main/java/com/sz/blinddata/domain/Province.java
@@ -0,0 +1,31 @@
+package com.sz.blinddata.domain;
+
+public class Province {
+    private Integer id;
+    private Integer pid;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
