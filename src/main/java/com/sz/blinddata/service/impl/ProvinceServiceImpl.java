eeb163c57a98231179083a9eec85f198a9be5ffa
wjz <25348903334@qq.com>
2020-03-02 16:36:58 +0800

    更改ProvinceServiceImpl类添加getFirstProvince和getSecondAndThirdProvince方法

--- src/main/java/com/sz/blinddata/service/impl/ProvinceServiceImpl.java
+++ src/main/java/com/sz/blinddata/service/impl/ProvinceServiceImpl.java
@@ -3,6 +3,7 @@ package com.sz.blinddata.service.impl;
 import com.sz.blinddata.domain.Province;
 import com.sz.blinddata.mapper.ProvinceMapper;
 import com.sz.blinddata.service.ProvinceService;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -11,6 +12,8 @@ import java.util.List;
 public class ProvinceServiceImpl implements ProvinceService {
     @Resource
     private ProvinceMapper provinceMapper;
+    @Resource
+    private RedisTemplate redisTemplate;
     @Override
     public List<Province> getPidName() {
         return provinceMapper.getPidName();
@@ -20,4 +23,15 @@ public class ProvinceServiceImpl implements ProvinceService {
     public List<Province> getProvinceListById(Province province) {
         return provinceMapper.getProvinceListById(province);
     }
+
+    @Override
+    public List<Province> getFirstProvince() {
+        return redisTemplate.opsForList().range("province",0,-1);
+    }
+
+    @Override
+    public List<Province> getSecondAndThirdProvince(Province province) {
+        return (List<Province>)redisTemplate.opsForHash().get("provinceMap",province.getName());
+    }
+
 }
